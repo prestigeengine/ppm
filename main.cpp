6708adf93bb65c575e57e3f7e43ba261659898a7
Leandro170 <73649765+Leandro-170@users.noreply.github.com>
2022-06-18 16:34:03 -0300

    shit stuff

--- main.cpp
+++ main.cpp
@@ -1,7 +1,66 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <fstream>
 
-int main(int count, char* value[])
+const std::string ver = "v0.01";
+
+void pkginstall(const char* pkg)
 {
-	std::cout << "i love frums" << std::endl; // IMPORTANT: endl calls os.flush()!!!
-	return 0;
+	// TODO: fetch from website
+	std::fstream package(pkg, std::ios::out);
+	if (!package)
+	{
+		std::cerr << "Failed to open package!\n";
+		exit(1);
+	}
+	// TODO: deal with package properly
+	package.close();
+}
+
+int printInstallHelp(void)
+{
+	std::cerr
+		<< "PPM v0.01\n"
+		<< "No package specified!"
+		<< "Usage: ppm install <package>";
+	return EXIT_FAILURE;
+}
+
+void do_stuff(std::string do_this, const char* with)
+{
+	if (do_this.compare("install")) pkginstall(with);
+}
+
+void cease() {}
+
+int main(int count, const char* value[])
+{
+	if (count < 1) // no action specified
+	{
+		std::cerr << "PPM " << ver << "\nNo action specified!\n";
+		exit(1);
+	}
+
+	if (strcmp(value[1], "version") == 0)
+	{
+		std::cout << "PPM " << ver;
+		exit(0);
+	}
+	if (count < 3) // no thing to mess with!
+	{
+		if (strcmp(value[1], "install") == 0)
+		{
+			std::cerr
+				<< "PPM " << ver
+				<< "\nNo package specified!"
+				<< "\nUsage: ppm install <package>"
+				<< std::endl;
+		}
+		exit(1);
+	}
+	do_stuff(value[1], value[2]);
+
+	cease();
 }
